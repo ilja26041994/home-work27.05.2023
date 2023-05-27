6881a803d470bad0b948ce5b2c7b023610044516
ilja26041994 <134002146+ilja26041994@users.noreply.github.com>
2023-05-28 00:17:10 +0300

    6.6

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
-# 6.4
-count = 0
+# # 6.4
+# count = 0
+# while True:
+#     n = int(input('some negative number '))
+#     count += 1
+#     if n > 0:
+#         break
+# print('quantity of negative numbers', count)
+# 6.6
+cnt = 0
+n = int(input('number n'))
 while True:
-    n = int(input('some negative number '))
-    count += 1
-    if n > 0:
+    a = int(input('number a'))
+    cnt += 1
+    if a < n:
+        x1 = 1 * a
+        x2 = 1 * cnt
+    if a > n and a != n: # неверное условие для определения числа следующего за числом н
+        x3 = 1 * a
+        x4 = 1 * cnt
+    if cnt == 5:
         break
-print('quantity of negative numbers', count)
+print('summa a < n =', a)
+print('the ordinal number of the number', x1, 'this', x2)
+print('the ordinal number of the number', x3, 'this', x4)
